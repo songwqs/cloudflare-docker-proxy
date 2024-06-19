0e6c691d22c5830d4bdd40c7617b69659ea046c6
songwqs <songwqs@gmail.com>
2024-06-19 17:04:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.songwqs.top": dockerHub,
+  "quay.songwqs.top": "https://quay.io",
+  "gcr.songwqs.top": "https://gcr.io",
+  "k8s-gcr.songwqs.top": "https://k8s.gcr.io",
+  "k8s.songwqs.top": "https://registry.k8s.io",
+  "ghcr.songwqs.top": "https://ghcr.io",
+  "cloudsmith.songwqs.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.songwqs.top": dockerHub,
 };
 
 function routeByHosts(host) {
